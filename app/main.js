55f06470f9ea3e2d3c1eef230dce42d02e13c083
Wang Jiashu <wjsvec@163.com>
2023-10-29 20:54:05 +0800

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -5,6 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+    socket.on("data", (data) => {
+        print(data)
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        socket.end();
+1
+    });
   socket.on("close", () => {
     socket.end();
     server.close();
