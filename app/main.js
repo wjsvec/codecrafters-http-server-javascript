175cf1aa3ddd84c759a052007876242ba9289353
Wang Jiashu <wjsvec@163.com>
2023-10-29 22:13:55 +0800

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,4 +1,14 @@
 const net = require("net");
+const fs = require('fs');
+let dir;
+if (process.argv.length > 2 && process.argv[2] == "--directory") {
+  dir = process.argv[3];
+  if (!dir.endsWith("/")) {
+    dir = dir + "/";
+  }
+1
+}
+
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -6,12 +16,12 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        console.log(String(data).split("\r\n"))
+        
         if(String(data).split("\r\n")[0].split(" ")[1] =="/"){
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         }
         else if(String(data).split("\r\n")[0].split(" ")[1].slice(0,5) =="/echo"){
-            var res = ["HTTP/1.1 200 OK",
+            let res = ["HTTP/1.1 200 OK",
                 "Content-Type: text/plain",
                 "Content-Length: "+String((String(data).split("\r\n")[0].split(" ")[1].slice(5,)).length -1)+"\r\n",           
                 String(data).split("\r\n")[0].split(" ")[1].slice(6,)];
@@ -24,8 +34,12 @@ const server = net.createServer((socket) => {
                 "Content-Length: "+String((String(data).split("\r\n")[2].split(" ")[1]).length )+"\r\n",           
                 (String(data).split("\r\n")[2].split(" ")[1])];
             // console.log(res)
-            
+
             socket.write(res.join("\r\n"))
+
+        }
+        else if(String(data).split("\r\n")[0].split(" ")[1].slice(0,6) =="/files"){
+            console.log(String(data).split("\r\n"))
 
         }
         else{
