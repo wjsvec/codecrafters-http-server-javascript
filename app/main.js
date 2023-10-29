349b4123be972c1f41aa9e6acc9e7eb9f6fdd4f8
Wang Jiashu <wjsvec@163.com>
2023-10-29 21:30:49 +0800

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ const server = net.createServer((socket) => {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         }
         else if(String(data).split("\r\n")[0].split(" ")[1].slice(0,5) =="/echo"){
-            var res = ["HTTP/1.1 200 OK",
+            var res = ["HTTP/1.1 200 OK\r\n",
                 "Content-Type: text/plain",
                 "Content-Length: "+String((String(data).split("\r\n")[0].split(" ")[1].slice(5,)).length),           
                 String(data).split("\r\n")[0].split(" ")[1].slice(5,)];
