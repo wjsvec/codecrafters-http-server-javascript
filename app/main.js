03c64092a2802c347bcf3f428ee30b747a7d2e52
Wang Jiashu <wjsvec@163.com>
2023-10-29 22:16:41 +0800

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -40,6 +40,7 @@ const server = net.createServer((socket) => {
         }
         else if(String(data).split("\r\n")[0].split(" ")[1].slice(0,6) =="/files"){
             console.log(String(data).split("\r\n"))
+            console.log(dir)
             fs.readFile(dir, 'utf8', (err, data) => {
                 if (err) {
                   console.error('无法读取文件:', err);
