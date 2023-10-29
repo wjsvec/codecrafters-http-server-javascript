c6fae6a26b4fd7a5daad712e0efb1b84465616f7
Wang Jiashu <wjsvec@163.com>
2023-10-29 21:10:45 +0800

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -6,8 +6,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        console.log(String(data).split("\r\n")[0].split(" ")[1]+"   --")
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        if(String(data).split("\r\n")[0].split(" ")[1] =="/"){
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        }
+        else{
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        
         socket.end();
 1
     });
