d2b5c1075ed5094d2a9311635d171b9cbd040340
Wang Jiashu <wjsvec@163.com>
2023-10-29 22:42:30 +0800

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -59,7 +59,7 @@ const server = net.createServer((socket) => {
 
             
             // console.log(dir+String(data).split("\r\n")[0].split(" ")[1].slice(7))
-            return;
+            // return;
 
         }
         else{
